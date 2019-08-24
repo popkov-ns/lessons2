f6321d62fead592be326ac42e74254312723643e
Nikita Popkov <popkov-ns@mail.ru>
2019-08-24 16:26:23 +0300

    Изменение script основного задания

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-let money, income, addExpenses, deposit, mission, period;
+let money = 50000,
+    income = 'Фриланс - 30000 рублей в месяц',
+    addExpenses = 'Дорога, Машина, Еда',
+    deposit = true,
+    mission = 300000,
+    period = 12;
 
-alert('Любой текст');
+// Вывести в консоль тип данных значений переменных money, income, deposit;
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-console.log(100700);
+// Вывести в консоль длину строки income
+console.log(income.length);
+
+// Вывести в консоль “Период (period) месяцев” и “Цель заработать (mission) рублей”
+console.log('Период' + ' ' + period + ' ' + 'месяцев');
+console.log('Цель заработать' + ' ' + mission + ' ' + 'рублей');
+
+// Привести строку addExpenses к нижнему регистру и разбить строку на массив, вывести массив в консоль
+console.log(addExpenses.toLowerCase().split(', '));
+
+// Объявить переменную budgetDay и присвоить дневной бюджет, вывести в консоль результат и остаток от деления
+let budgetDay;
+console.log(budgetDay = money / 30);
+console.log(budgetDay = money % 30);
